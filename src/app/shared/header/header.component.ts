b900f70f6b058e5911c69a52ffbc78c86625aded
Francisco <franhdez97@gmail.com>
2021-08-03 13:07:38 -0600

    cambios finales

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public showOptions = "header__mobile";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public showMenu() {
+    this.showOptions = this.showOptions === "header__mobile" ? "header__mobile--active" : "header__mobile";
   }
 
 }
